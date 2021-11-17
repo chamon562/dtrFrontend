da617ba4bddf47f003be0a6c16c8d8d235da7b88
unknown <Chamon562@gmail.com>
2021-11-17 09:48:14 -0500

    Initial set up with components folder, page folder, hHome.jsx file in Pages and imported Home to app.js to see basic greeting of website.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Rank turbo</h1>
+    <div>
+    <Home />
     </div>
   );
 }
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+const Home = () => {
+    return (
+        <div>
+            <h1>Dota 2 Ranked Turbo</h1>
+            <h1>Lets get these points</h1>
+            <p>Enter Friend Id to check your rank</p>
+            <TextField />
+        </div>
+    )
+}
+
+export default Home
