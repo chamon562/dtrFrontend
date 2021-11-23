cc379aa6bcec2faac9c6951d7f6610b0202dce23
unknown <Chamon562@gmail.com>
2021-11-23 13:53:40 -0500

    [Navbar.jsx] Add hamburger icon in navbar when user reduces window size and installed boostrap icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import ModalForm from "../Modal/ModalForm";
-import Home from "../Pages/Home";
+import PetsIcon from '@mui/icons-material/Pets';
+// import Home from "../Pages/Home";
+import About from "../Pages/About"
 import { NavLink, Link } from "react-router-dom";
 const Navbar = () => {
-  return(
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand"> Home </Link>
+        <Link className="navbar-brand" to="/">
+          <PetsIcon />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample07"
+          aria-controls="#navbarsExample07"
+          adia-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
       </div>
     </nav>
-  ) 
+  );
 };
 
 export default Navbar;
