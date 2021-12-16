f1b7725820169cda870a6e18214e69a0f8a0d0b0
unknown <Chamon562@gmail.com>
2021-12-16 10:25:59 -0500

    Add state for to save whether the user was radient or dire based on the getting the information from player_slot and save that in the state to be used for if logic later.

--- src/components/Pages/profile/ProfileDotaStateOne.jsx
+++ src/components/Pages/profile/ProfileDotaStateOne.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 const ProfileDotaStateOne = (props) => {
   const [dotaMatches, setDotaMatches] = useState([]);
-
+  const [radient, setRadient] = useState("");
+  const [dire, setDire] = useState("");
   useEffect(() => {
     axios
       .get(`/api/players/${props.userData.friendId}/recentMatches`)
@@ -11,18 +12,18 @@ const ProfileDotaStateOne = (props) => {
         console.log(error);
       });
   }, []);
-  const playSlot = dotaMatches[0].player_slot
+  const playSlot = dotaMatches[0].player_slot;
   console.log(props.userData.friendId);
   console.log(playSlot);
-  if(playSlot <= 127){
-      console.log("you are radient")
+  if (playSlot <= 127) {
+    console.log("you are radient");
   } else {
-      console.log(" you are dire homie")
+    console.log(" you are dire homie");
   }
   //   player_slot:  Which slot the player is in. 0-127 are Radiant, 128-255 are Dire
-//   if(dotaMatches[0].player_slot <= 127){
-//       console.log(`you are radient`)
-//   }
+  //   if(dotaMatches[0].player_slot <= 127){
+  //       console.log(`you are radient`)
+  //   }
   return <div>profile dota stats</div>;
 };
 
